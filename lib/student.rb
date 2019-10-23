4a87e5cb854aec7389599d23de5f2e93630d2c84
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-23 15:57:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
   def self.all_students_in_grade_9
     sql = <<-SQL
          SELECT * FROM students
-         WHERE grade < 12
+         WHERE grade = 9
        SQL
 
        DB[:conn].execute(sql)
