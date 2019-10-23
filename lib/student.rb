d62dbdb036f1353616b81ecdac4814b0ab459bd1
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-23 16:58:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,8 @@ end
          LIMIT 1
        SQL
 
-       DB[:conn].execute(sql)
+       DB[:conn].execute(sql).map do |row|
+         self.new_from_db(row)
   end
 
   def self.all_students_in_grade_X(grade)
