419e4b99d74e5024d3243abcf5bb536e35ced1a6
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-23 15:57:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,12 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+         SELECT * FROM students
+         WHERE grade < 12
+       SQL
+
+       DB[:conn].execute(sql)
   end
 
   def self.first_X_students_in_grade_10
