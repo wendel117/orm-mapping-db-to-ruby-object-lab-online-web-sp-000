1768439f71d437035d8c30c97bd71745538648d0
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-23 16:18:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,6 +58,13 @@ class Student
   end
 
   def self.first_student_in_grade_10
+    sql = <<-SQL
+     SELECT * FROM students
+     WHERE grade = 12
+     LIMIT 1
+   SQL
+
+   DB[:conn].execute(sql)
   end
 
   def self.all_students_in_grade_X(grade)
